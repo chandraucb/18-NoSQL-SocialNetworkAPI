d8222866cfae2313204b1d4f1163ab9ab719e866
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-09-14 13:23:35 -0700

    Formatting update

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,34 +1,35 @@
-const router = require('express').Router();
-const { 
-    getAllThoughts, 
-    getThoughtById, 
-    createThought, 
-    updateThought,
-    addReaction,
-    deleteThought,
-    deleteReaction
-} = require('../../controllers/thought-controller');
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  addReaction,
+  deleteThought,
+  deleteReaction,
+} = require("../../controllers/thought-controller");
 
 // Get through - /api/thoughts
-router.route('/')
-      .get(getAllThoughts);
+router.route("/")
+  .get(getAllThoughts);
 
 // get/put/delete thought - /api/thoughts/:id
-router.route('/:id')
-      .get(getThoughtById)
-      .put(updateThought)
-      .delete(deleteThought); 
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
 
 // create thought - /api/thoughts/:userId
-router.route('/:userId')    
-      .post(createThought);
+router.route("/:userId")
+  .post(createThought);
 
 // add reaction - /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions')
-      .post(addReaction);
+router.route("/:thoughtId/reactions")
+  .post(addReaction);
 
 // delete reaction - /api/thoughts/:thoughtId/reactionId
-router.route('/:thoughtId/reactions/:reactionId')
-      .delete(deleteReaction);
+router.route("/:thoughtId/reactions/:reactionId")
+  .delete(deleteReaction);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,31 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
-    getAllUsers, 
-    getUserById, 
-    createUser, 
-    updateUser, 
-    deleteUser, 
-    addFriend, 
-    deleteFriend 
-  } = require('../../controllers/user-controller');
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/user-controller");
 
 // get users, create user - /api/users
-router
-  .route('/')
+router.route("/")
   .get(getAllUsers)
   .post(createUser);
 
 // get,put,delete user - /api/users/:id
-router
-  .route('/:id')
+router.route("/:id")
   .get(getUserById)
-  .put(updateUser)  
+  .put(updateUser)
   .delete(deleteUser);
 
-  // Setup add Friend and delete Friend at /api/users/:userId/friends/:friendId
-router
-.route('/:id/friends/:friendId')
-.put(addFriend)
-.delete(deleteFriend)
+// Setup add Friend and delete Friend at /api/users/:userId/friends/:friendId
+router.route("/:id/friends/:friendId")
+  .put(addFriend)
+  .delete(deleteFriend);
 
 module.exports = router;
